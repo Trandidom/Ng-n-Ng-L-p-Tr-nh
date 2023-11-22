c5b58587270330368352a62f40065b9fa722cf02
Trandidom <151102568+Trandidom@users.noreply.github.com>
2023-11-22 10:04:03 +0700

    Add files via upload

---
+++ jdoodle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Ho va ten: So Hao Ha My Tran\n");
+	printf("Ngay thang nam sinh: 10/01/2005\n");
+	printf("Hoc tai: Truong cao dang ky thuat cong nghe nha trang - lop: cd-cntt17a\n");
+
+	
+	return 0;
+}
